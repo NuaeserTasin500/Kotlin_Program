9406d5fbcf9046a4df3b2f0958074890e80c8706
NuaeserTasin500 <osman1711362@gmail.com>
2025-11-21 22:51:44 +0600

    Create 003. Variables (With Data Types).kt

---
+++ 003. Variables (With Data Types).kt
@@ -0,0 +1,14 @@
+fun main()
+{
+    // var allows to change value. val doesn't allow allow to change value 
+    var nameVar: String = "Tasin" 
+    var age: Int = 22
+    println("Hello! My name is $nameVar. My age is $age")
+
+    nameVar = "Rahim"
+    println("Hello! My name is $nameVar. My age is $age")
+    
+    val piName: String = "Pi Value"
+    val pi: Double = 3.1416
+    println("Hello! I am $piName. My value is $pi")
+}
