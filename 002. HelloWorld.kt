d738b9ba8bdd19be1ec411d9b703a533c85f8046
NuaeserTasin500 <osman1711362@gmail.com>
2025-11-22 22:57:39 +0600

    Update 002. HelloWorld.kt

--- 002. HelloWorld.kt
+++ 002. HelloWorld.kt
@@ -1,4 +1,9 @@
 fun main()
 {
+    //// Using println (create newline after printing the string)
     println("Hello World")
+
+    //// Using print (never create newline after printing the string)
+    print("Hello ")
+    print("World")
 }
